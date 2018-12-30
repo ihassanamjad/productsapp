ede0ab537661f5fe709c541d0de5cf97e7b18ade
Hassan Amjad <hassanamjad77@gmail.com>
2018-12-30 16:07:33 +0500

    Defining Product Route

---
+++ app/routes/products.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+
+//Requiring a controller which is not defined yet
+const productController = require('../controllers/products')
+
+//A simple Test to check if our controller is communicating fine with our files
+router.get('/test', productController.test);
+
+module.exports = router;
