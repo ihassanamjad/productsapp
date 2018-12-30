02f289e61c323760f2dcb56dad9a406e27248f8e
Hassan Amjad <hassanamjad77@gmail.com>
2018-12-30 16:26:17 +0500

    Added product controller and defined routes in app.js

--- app.js
+++ app.js
@@ -5,6 +5,12 @@ const bodyParser = require('body-parser');
 const app = express();
 let port = 9000;
 
+//Importing the Routes 
+const productRoute = require('./app/routes/products');
+
+//Using the Routes
+app.use('/products', productRoute);
+
 //Listening Express application at port 9000
 app.listen(9000, () => {
     console.log('Server is up and running at port ' +  port)
---
+++ app/controllers/products.js
@@ -0,0 +1,6 @@
+const Product = require('../models/products')
+
+
+exports.test = function(req, res) {
+    res.send('Greetings from the Test Controller')
+}
